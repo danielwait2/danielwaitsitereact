68d4d1a236c9665b1c6b87965ff76569e364789d
Wait Works <waitworks@Daniels-MacBook-Air-185.local>
2025-08-08 15:14:59 -0400

    Add dropdown navigation for About Me with Resume, Projects, and Contact sub-items

--- public/js/nav-component.js
+++ public/js/nav-component.js
@@ -9,12 +9,19 @@ function loadNavigation() {
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav ms-auto mb-2 mb-lg-0 small fw-bolder">
-                            <li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
-                            <li class="nav-item"><a class="nav-link" href="resume.html">Resume</a></li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="aboutDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    About Me
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="aboutDropdown">
+                                    <li><a class="dropdown-item" href="index.html">About Me</a></li>
+                                    <li><a class="dropdown-item" href="resume.html">Resume</a></li>
+                                    <li><a class="dropdown-item" href="projects.html">Projects</a></li>
+                                    <li><a class="dropdown-item" href="contact.html">Contact</a></li>
+                                </ul>
+                            </li>
                             <li class="nav-item"><a class="nav-link" href="wait-works.html">Wait Works LLC</a></li>
-                            <li class="nav-item"><a class="nav-link" href="projects.html">Projects</a></li>
                             <li class="nav-item"><a class="nav-link" href="wait-list.html">Wait List</a></li>
-                            <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
                         </ul>
                     </div>
                 </div>
